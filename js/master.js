337b2ba79ad4c7f7d74c8ee53fa56284209e8025
SungWSC <120364448+SungWSC@users.noreply.github.com>
2024-09-24 21:19:48 +0800

    新增頁面

---
+++ js/master.js
@@ -0,0 +1,40 @@
+// 寫法一 -----------------------------------------------
+// let isMobile;
+
+// if($(window).width() <= 480) {
+//     isMobile = true;
+// }else {
+//     isMobile = false
+// }
+
+// console.log(isMobile);
+
+
+// 寫法二 -----------------------------------------------
+// console.log($(window).width() <= 480);
+const isMobile = $(window).width() <= 480;
+// console.log(isMobile);
+
+const $grid = $('#Grid')
+
+
+function setGrid() {
+    if (isMobile === true) {
+        $grid.addClass('grid-2 gap-small')
+    } else {
+        $grid.addClass('grid-3 gap-medium')
+    }
+}
+// 補充:
+// = 賦值
+// == 判斷是否相同
+// === 判斷是否相同 (包含型別)
+
+function setInit(){
+    setGrid();
+}
+
+
+// init --------------------------------------------------------------
+
+setInit();
